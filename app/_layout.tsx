b3a647d149a764446ddbbcdcc0c2fc77c5b86e18
BuildTools <unconfigured@null.spigotmc.org>
2025-07-07 21:35:23 +0700

    Setup Expo Router

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,5 @@
-import { Stack } from "expo-router";
+import { Stack, Slot } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Slot />;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,6 @@
+import { Redirect } from "expo-router";
 import { Text, View } from "react-native";
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello world</Text>
-    </View>
-  );
+export default function HomeScreen() {
+  return <Redirect href={'/(home)/(tabs)' as any} />;
 }
