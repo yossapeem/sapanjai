98679dd5aba6bca7de49f7d8b8bf308665f1d692
BuildTools <unconfigured@null.spigotmc.org>
2025-07-12 16:12:22 +0700

    Supabase Authentication

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,5 +2,5 @@ import { Redirect } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function HomeScreen() {
-  return <Redirect href={'/(home)/(tabs)' as any} />;
+  return <Redirect href={'/(auth)/Login' as any} />;
 }
