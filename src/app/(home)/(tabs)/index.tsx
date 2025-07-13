f5f9a42c6c93d82564e3dc26384b0eb5ff3be5da
BuildTools <unconfigured@null.spigotmc.org>
2025-07-13 16:47:15 +0700

    Connect user avatar to stream chat

--- src/app/(home)/(tabs)/index.tsx
+++ src/app/(home)/(tabs)/index.tsx
@@ -1,10 +1,14 @@
+import { useAuth } from "@/src/providers/AuthProvider";
 import { router } from "expo-router";
 
 import { ChannelList } from "stream-chat-expo";
 
 export default function MainTab() {
+  const { user } = useAuth();
+
   return (
     <ChannelList
+      filters={{members: {$in: [user.id]}}}
       onSelect={(channel) => router.push(`/channel/${channel.cid}` as any)}
     />
   );
--- src/providers/ChatProvider.tsx
+++ src/providers/ChatProvider.tsx
@@ -4,28 +4,33 @@ import { ActivityIndicator } from "react-native";
 import { StreamChat } from "stream-chat";
 import { Chat, OverlayProvider } from "stream-chat-expo";
 import { useAuth } from "./AuthProvider";
+import { supabase } from "../lib/supabase";
 
 const client = StreamChat.getInstance(process.env.EXPO_PUBLIC_STREAM_API_KEY!);
 
 export default function ChatProvider({ children }: PropsWithChildren) {
   const [isActive, setIsActive] = useState(false);
   const { profile } = useAuth();
-  
+
   useEffect(() => {
-    console.log("USE EFFECT: ", profile)
+    console.log("USE EFFECT: ", profile);
     if (!profile) {
       return;
     }
     const connect = async () => {
       console.log(profile.id);
+
       await client.connectUser(
         {
           id: profile.id,
           name: profile.full_name,
-          image: "https://i.imgur.com/fR9Jz14.png",
-        },
+          image: supabase.storage
+            .from("avatars")
+            .getPublicUrl(profile.avatar_url).data.publicUrl as any,
+        } ,
         client.devToken(profile.id)
       );
+
       setIsActive(true);
       //  const channel = client.channel("messaging", "the_park", {
       //    name: "The Park",
