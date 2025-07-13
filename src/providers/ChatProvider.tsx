714a94fd80e2dbddad5de0816d2052135e038556
BuildTools <unconfigured@null.spigotmc.org>
2025-07-13 16:33:22 +0700

    Connect supabase user to streamchat user

--- src/providers/ChatProvider.tsx
+++ src/providers/ChatProvider.tsx
@@ -3,21 +3,28 @@ import { useEffect } from "react";
 import { ActivityIndicator } from "react-native";
 import { StreamChat } from "stream-chat";
 import { Chat, OverlayProvider } from "stream-chat-expo";
+import { useAuth } from "./AuthProvider";
 
 const client = StreamChat.getInstance(process.env.EXPO_PUBLIC_STREAM_API_KEY!);
 
 export default function ChatProvider({ children }: PropsWithChildren) {
-  const [isAcitive, setIsActive] = useState(false);
-
+  const [isActive, setIsActive] = useState(false);
+  const { profile } = useAuth();
+  
   useEffect(() => {
+    console.log("USE EFFECT: ", profile)
+    if (!profile) {
+      return;
+    }
     const connect = async () => {
+      console.log(profile.id);
       await client.connectUser(
         {
-          id: "jlahey",
-          name: "Jim Lahey",
+          id: profile.id,
+          name: profile.full_name,
           image: "https://i.imgur.com/fR9Jz14.png",
         },
-        client.devToken("jlahey")
+        client.devToken(profile.id)
       );
       setIsActive(true);
       //  const channel = client.channel("messaging", "the_park", {
@@ -29,12 +36,14 @@ export default function ChatProvider({ children }: PropsWithChildren) {
     connect();
 
     return () => {
-      client.disconnectUser();
+      if (isActive) {
+        client.disconnectUser();
+      }
       setIsActive(false);
     };
-  }, []);
+  }, [profile?.id]);
 
-  if (!isAcitive) {
+  if (!isActive) {
     return <ActivityIndicator />;
   }
 
