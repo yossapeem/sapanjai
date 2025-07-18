ff3abcfa6a6a279f23a2d154505afc3b5e3b362c
BuildTools <unconfigured@null.spigotmc.org>
2025-07-18 17:55:11 +0700

    final version

--- src/utils/tokenProvider.tsx
+++
@@ -1,7 +0,0 @@
-import { supabase } from "../lib/supabase";
-
-export const tokenProvider = async () => {
-  const { data } = await supabase.functions.invoke("stream-token");
-  console.log(data);
-  return data.token;
-};
